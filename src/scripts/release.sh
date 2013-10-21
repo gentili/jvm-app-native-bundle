ba6b2632424a03af99b99771169dabaab7b42113
gentili <gentili@8c7f07c7-4e7d-0410-9ed6-aab42a9a149d>
2013-10-21 01:04:15 +0000

    Change Launcher filename and roll up to V1.0.1
    git-svn-id: svn://mcpnet.ca/demurrage/GameLauncher@691 8c7f07c7-4e7d-0410-9ed6-aab42a9a149d

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -1,7 +1,8 @@
 export TARGET="/home/gentili/videogamez.ca/htdocs/demurrage/GameLauncher/"
 export RELEASE="${releaseName}"
+export NAME="DemurrageLauncher.jar"
 echo Installing ${RELEASE} to videogamez.ca...
 scp *.TXT gentili@mcpnet.ca:${TARGET}
 scp ../${RELEASE} gentili@mcpnet.ca:videogamez.ca/htdocs/demurrage/GameLauncher/
-ssh gentili@mcpnet.ca "rm ${TARGET}GameLauncher.jar"
-ssh gentili@mcpnet.ca "ln -s ${TARGET}${RELEASE} ${TARGET}GameLauncher.jar"
+ssh gentili@mcpnet.ca "rm ${TARGET}${NAME}"
+ssh gentili@mcpnet.ca "ln -s ${TARGET}${RELEASE} ${TARGET}${NAME}"
