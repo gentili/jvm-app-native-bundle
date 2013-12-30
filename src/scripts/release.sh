53541259266365d41a29a76646d5252b9b6013e8
gentili <gentili@8c7f07c7-4e7d-0410-9ed6-aab42a9a149d>
2013-12-30 02:06:04 +0000

    Now generate a windows exe for the launcher
    git-svn-id: svn://mcpnet.ca/demurrage/GameLauncher@758 8c7f07c7-4e7d-0410-9ed6-aab42a9a149d

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -1,8 +1,13 @@
 export TARGET="/home/gentili/videogamez.ca/htdocs/demurrage/GameLauncher/"
 export RELEASE="${releaseName}"
-export NAME="DemurrageLauncher.jar"
+export EXENAME="${exeName}"
+export FINALJARNAME="DemurrageLauncher.jar"
+export FINALEXENAME="DemurrageLauncher.exe"
 echo Installing ${RELEASE} to videogamez.ca...
 scp *.TXT gentili@mcpnet.ca:${TARGET}
 scp ../${RELEASE} gentili@mcpnet.ca:videogamez.ca/htdocs/demurrage/GameLauncher/
-ssh gentili@mcpnet.ca "rm ${TARGET}${NAME}"
-ssh gentili@mcpnet.ca "ln -s ${TARGET}${RELEASE} ${TARGET}${NAME}"
+scp ../${EXENAME} gentili@mcpnet.ca:videogamez.ca/htdocs/demurrage/GameLauncher/
+ssh gentili@mcpnet.ca "rm ${TARGET}${FINALJARNAME}"
+ssh gentili@mcpnet.ca "ln -s ${TARGET}${RELEASE} ${TARGET}${FINALJARNAME}"
+ssh gentili@mcpnet.ca "rm ${TARGET}${FINALEXENAME}"
+ssh gentili@mcpnet.ca "ln -s ${TARGET}${EXENAME} ${TARGET}${FINALEXENAME}"
